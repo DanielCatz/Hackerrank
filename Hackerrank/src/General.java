135f469e7b69a669603d660ec909f595a1ea98aa
Daniel Caterson <daniel.caterson@gmail.com>
2017-05-01 15:48:36 -0700

    pairs that sum
    Sorted array version
    Time: (N), Space: (1)

--- Hackerrank/src/General.java
+++ Hackerrank/src/General.java
@@ -2,8 +2,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class General {
+	/***
+	 * Find the most frequent integer in an array
+	 * @param arr
+	 * @return
+	 */
 	public static int mostFrequentIntegerInArray(int[] arr){
 		HashMap<Integer,Integer> solution = new HashMap<>();		
+		if(arr==null|| arr.length==0){
+			throw new IllegalArgumentException();
+		}
+		
 		for(int i = 0; i< arr.length;i++){
 			if(solution.containsKey(arr[i])){
 				solution.put(arr[i], solution.get(arr[i])+1);
@@ -22,6 +31,33 @@ public class General {
 		}		
 		return max;// Time: n, Space n
 	}
-	
-	
+	/***
+	 * Find pairs in an integer array whose sum is equal to 10 (bonus: do it in linear time)
+	 * assume unsorted
+	 * @param value
+	 * @param arr
+	 * @return
+	 */
+	public static int[] findPairThatSumTo(int value, int[] arr){
+		
+		//if sorted, work my way towards the middle
+		int start = 0;
+		int end = arr.length-1;
+		while(start<end){
+			if(arr[start]+arr[end] == value){
+				int[] solution ={arr[start],arr[end]};
+				return solution;
+			}else if(arr[start]+arr[end]< value){
+				start++;
+			}else{
+				end--;
+			}
+		}
+		
+		
+		
+		
+		
+		return null;
+	}
 }
--- Hackerrank/src/Unofficial.java
+++ Hackerrank/src/Unofficial.java
@@ -3,9 +3,11 @@ public class Unofficial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int[] sortedArr= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
 		int[] randomArr = {2,4,64,1,4,57,9,2,6,9,4,24,57,67,2,4,4,9,36,2};
-		System.out.println(General.mostFrequentIntegerInArray(randomArr));
-		System.out.println(General.mostFrequentIntegerInArray(new int[0]));
+		//System.out.println(General.mostFrequentIntegerInArray(randomArr));
+		System.out.println(General.findPairThatSumTo(13,sortedArr)[1]);
 	}
 
 }
